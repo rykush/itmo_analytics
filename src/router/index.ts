0596f1f4360d326a61d6f6f97441a7d6eb982814
rykush <actushima@gmail.com>
2026-04-08 10:14:59 +0300

    fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import type { RouteLocationNormalized } from 'vue-router'; // тип для хуков
+import type { RouteLocationNormalized } from 'vue-router';
 import BooksList from '@/pages/home/booksList.vue';
 import FormAdd from '@/features/add-book/formAdd.vue';
-import FormEdit from '@/features/edit-book/formEdit.vue'; // если есть
+import FormEdit from '@/features/edit-book/formEdit.vue';
 import Welcome from '@/pages/welcome/Welcome.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', component: BooksList },
+    { path: '/', component: Welcome },
     { path: '/add', component: FormAdd },
     { path: '/edit/:id', component: FormEdit },
     { path: '/welcome', component: Welcome }
   ]
 });
 
-// Хук после каждого перехода
+
 router.afterEach((to: RouteLocationNormalized, from: RouteLocationNormalized) => {
   const ymFunction = (window as any).ym;
 
   if (ymFunction && typeof ymFunction === 'function' && to.path !== from.path) {
-    console.log('Яндекс Метрика: отправка hit для', to.fullPath); // для отладки
+    console.log('Яндекс Метрика: отправка hit для', to.fullPath);
     ymFunction(108437493, 'hit', to.fullPath);
   }
 });
